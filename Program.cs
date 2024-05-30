5ffa095df5d18b191b6015b342e83aed16d45ac2
Ethan <Ethanlukandwa@gmail.com>
2024-05-30 15:30:14 -0500

    Starting a game called Matatu

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FiguringCardGame
+{
+    class Program{
+        static void Main(string[] args){
+            Random rand = new Random();
+
+            int num = rand.Next(1,53);
+            
+
+            Console.WriteLine(Planets.Earth + " is a planet");
+
+        }
+
+        enum Planets{
+            Mercury,
+            Venus,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Uranus,
+            Neptune,
+            Pluto
+        }
+    }
+}
+
